fb9b26a12758e6ef471e17b963658f41d3ec2585
junglee1597 <jungwoo.lee1@my.jcu.edu.au>
2019-04-02 15:56:50 +1000

    It commits to display layer of list of travel tracker lists.

--- assignment1.py
+++ assignment1.py
@@ -30,13 +30,76 @@ def welcome_message(user_name):
     if len(user_name) == 0:
         return "Input can not be blank"
     else:
-        return 'Travel Tracker 1.0 - By ,{}'.format(user_name.capitalize())
+        return 'Travel Tracker 1.0 - By {}'.format(user_name.capitalize())
+
+
+def max_city_width(trackers):
+    result = 0
+    for row in trackers:
+        if len(row[0]) > result:
+            result = len(row[0])
+    return result + 1
+
+
+def max_country_width(trackers):
+    result = 0
+    for row in trackers:
+        if len(row[1]) > result:
+            result = len(row[1])
+    return result + 1
+
+
+def display_places(trackers):
+    city_width = max_city_width(trackers)
+    country_width = max_country_width(trackers)
+
+    not_visited_count = 0
+    for i in range(0, len(trackers)):
+        row = trackers[i]
+        if row[3] == 'n':
+            print('*', end='')
+            not_visited_count += 1
+        else:
+            print('', end=' ')
+        print(i + 1, end='. ')
+        print("{:{city}}{:{country}}".format(row[0], "in " + row[1], city=city_width,
+                                             country=country_width + 3), end='')
+        print("Priority: {}".format(row[2]))
+
+    if not_visited_count == 0:
+        print(len(trackers), "places. No places left to visit. Why not add a new place?")
+    else:
+        print(len(trackers), "places. You still want to visit", not_visited_count, "places.")
 
 
 def main():
     user_name = str(input("Please Enter Your Name:"))
     print(welcome_message(user_name))
     trackers = read_data('places.csv')
+
+    while True:
+        menu = """ -----MENU------
+L - List place
+A - Add New Place
+M - Mark a place as visited
+Q - Quit"""
+        print(menu)
+        choice = input("-->").upper()
+
+        if choice == 'L':
+            display_places(trackers)
+
+        elif choice == 'A':
+            pass
+
+        elif choice == 'M':
+            pass
+
+        elif choice == 'Q':
+            pass
+
+        else:
+            print("Invalid menu choice")
 
 
 if __name__ == '__main__':
