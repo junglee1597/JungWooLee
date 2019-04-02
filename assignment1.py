742ca50e2f65984f09688dcc1a67bbd584b8ce9e
junglee1597 <jungwoo.lee1@my.jcu.edu.au>
2019-04-02 15:50:58 +1000

    It commits to import a csv file to python as a list of lists

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,42 @@
 """
-Replace the contents of this module docstring with your own details
-Name:
-Date started:
+Name:JUNG WOO LEE
+Date started: 27 MARCH 2019
 GitHub URL:
 """
+import csv
+
+
+def read_data(file_name):
+    trackers = []
+    with open(file_name, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            row[2] = int(row[2])
+            trackers.append(row)
+    print(len(trackers), "places loaded from", file_name)
+    return sort_list(trackers)
+
+
+def sort_list(lst):
+    lst = sorted(lst, key=sort_key)
+    return lst
+
+
+def sort_key(row):
+    return row[3], row[2]
+
+
+def welcome_message(user_name):
+    if len(user_name) == 0:
+        return "Input can not be blank"
+    else:
+        return 'Travel Tracker 1.0 - By ,{}'.format(user_name.capitalize())
 
 
 def main():
-    print("Travel Tracker 1.0 - by <Your Name>")
+    user_name = str(input("Please Enter Your Name:"))
+    print(welcome_message(user_name))
+    trackers = read_data('places.csv')
 
 
 if __name__ == '__main__':
